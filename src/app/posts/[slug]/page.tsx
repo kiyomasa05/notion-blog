b36a006e875984f5719010eceac696e730b35b54
kiyomasa05 <m230549@gmail.com>
2023-12-27 13:43:01 +0900

    page layout

---
+++ src/app/posts/[slug]/page.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Post = () => {
+  return (
+    <section className="container lg:px-2 px-5 h-screen lg:w-2/5 mx-auto mt-20">
+      <h2 className="w-full text-2xl font-medium">2kaime</h2>
+      <div className="border-b-2 w-1/3  mt-1 border-sky-900"></div>
+      <span className="text-grey-500">2021/22/11</span>
+      <br />
+      <p className="text-white bg-sky-900 rounded-xl font-medium mt-2 px-2 inline-block">
+        next.js
+      </p>
+      <div className="mt-10 font-medium">aaaaaa</div>
+    </section>
+  );
+};
+
+export default Post;
