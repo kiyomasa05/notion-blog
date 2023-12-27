42312517e5493e262b3f21d603b713e10d87db8b
kiyomasa05 <m230549@gmail.com>
2023-12-27 11:40:07 +0900

    single post作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default async function Home() {
             description={post.description}
             date={post.date}
             slug={post.slug}
-            tag={post.tags}
+            tags={post.tags}
           />
           {/* <p>{post.id}</p>
           <p>{post.title}</p> */}
--- src/components/Blog/SinglePost.tsx
+++ src/components/Blog/SinglePost.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 type Props = {
@@ -6,12 +7,27 @@ type Props = {
   description: string;
   date: string;
   slug: string;
-  tag: string;
+  tags: string[];
 };
 
 function SinglePost(props: Props) {
-  const { title, description, date, slug, tag } = props;
-  return <div>{title}</div>;
+  const { title, description, date, slug, tags } = props;
+  return (
+    <Link href={`/posts/${slug}`}>
+      <section className="lg:w-1/2 bg-sky-900 mb-8 mx-auto rounded-md p-5 shadow-2xl hover:shadow-none hover:translate-y-1 transition-all duration-300">
+        <div className="flex items-center gap-3">
+          <h2 className="text-gray-100 text-2xl font-medium mb-2">{title}</h2>
+          <div className="text-gray-100">{date}</div>
+          {tags.map((tag) => {
+            <span className="text-white bg-gray-500 rounded-xl px-2 pb-1 font-medium">
+              {tag}
+            </span>;
+          })}
+        </div>
+        <p className="text-gray-100">{description}</p>
+      </section>
+    </Link>
+  );
 }
 
 export default SinglePost;
