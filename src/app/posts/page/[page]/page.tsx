e5d1cacce53138e344d89419f5f1463b067a09a8
kiyomasa05 <m230549@gmail.com>
2024-11-13 08:08:03 +0900

    page/pageのISR化

--- src/app/posts/page/[page]/page.tsx
+++ src/app/posts/page/[page]/page.tsx
@@ -12,26 +12,35 @@ export const revalidate = 604800; // 1週間ごと
 /**
  * ビルド時にPostの数分、pageを作っておく
  * nextjs.org/docs/app/api-reference/functions/generate-static-params
- * @returns pages: {page: number}[]
+ * @returns [{ page: "1" }, { page: "2" }, { page: "3" }];
  */
 export async function generateStaticParams() {
+  // page数を取得
   const numberOfPage = await getNumberOfPages();
-  // page数分、配列を作る
-  let pages: { page: number }[] = Array(numberOfPage)
-    .fill(null)
-    .reduce((acc, _, i) => {
-      return [...acc, { page: i + 1 }];
-    }, []);
 
-  return { pages };
+  if (numberOfPage <= 0) {
+    return []; // ページ数が1ページ以下の場合はパスを生成しない
+  }
+  const pages: { page: string }[] = Array.from(
+    { length: numberOfPage },
+    (_, i) => ({
+      page: (i + 1).toString(),
+    })
+  );
+
+  return [...pages];
 }
-
 
 // NEXT15でのparamsの指定の仕方注意 ParamsをPromise型にしないとbuild時に型エラーになる
 //  https://nextjs.org/docs/app/building-your-application/upgrading/version-15#async-request-apis-breaking-change
-export default async function BlogPageList(params: Promise<{ page: number }>) {
+export default async function BlogPageListPost({
+  params,
+}: {
+  params: Promise<{ page: string }>;
+}) {
   const { page } = await params;
-  const postByPage = await getPostByPage(page);
+
+  const postByPage = await getPostByPage(parseInt(page));
   const numberOfPage = await getNumberOfPages();
   const allTags = await getAllTags();
 
